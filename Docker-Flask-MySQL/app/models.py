912cd6c30f11a7949fcb5624b9c7b21b2adced7f
yshnaider <87585400+yshnaider@users.noreply.github.com>
2021-08-04 13:50:38 +0300

    Add files via upload

---
+++ Docker-Flask-MySQL/app/models.py
@@ -0,0 +1,22 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Table(UserMixin, db.Model):
+    """
+    Create an URL table
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'urls'
+
+    id = db.Column(db.Integer, primary_key=True)
+    tinyURL = db.Column(db.String(100), unique=True, nullable=False)
+    url = db.Column(db.String(200), unique=True, nullable=False)
+
+    def __init__(self, tinyURL, url):
+        self.tinyURL = tinyURL
+        self.url = url
